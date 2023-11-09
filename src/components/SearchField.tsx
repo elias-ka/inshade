7f42a82ab508d33bce61ee69965461b87cd22602
elias-ka <48407293+elias-ka@users.noreply.github.com>
2023-11-09 14:09:56 +0200

    Refactor SearchField to not use filterOptions

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, TextField, createFilterOptions } from '@mui/material';
+import { Autocomplete, TextField } from '@mui/material';
 import { debounce } from 'lodash';
 import { useEffect, useMemo } from 'react';
 import { Place } from '../models/Address';
@@ -7,7 +7,7 @@ interface SearchFieldProps {
     label: string;
     loading: boolean;
     options: Place[] | undefined;
-    onChange(value: Place): void;
+    onChange: (place: Place | null) => void;
     onInput: (value: string) => void;
 }
 
@@ -18,42 +18,30 @@ export default function SearchField({
     onChange,
     onInput,
 }: SearchFieldProps) {
-    const debouncedChangeHandler = useMemo(() => {
+    const debouncedInputHandler = useMemo(() => {
         return debounce((value: string) => onInput(value), 500);
     }, [onInput]);
 
     useEffect(() => {
         return () => {
-            debouncedChangeHandler.cancel();
+            debouncedInputHandler.cancel();
         };
-    }, [debouncedChangeHandler]);
+    }, [debouncedInputHandler]);
 
     return (
         <Autocomplete
             freeSolo
-            clearOnEscape
-            autoHighlight
-            onChange={(_, value) => {
-                if (value) {
-                    const option = options?.find((o) => o.displayName === value);
-                    if (option) {
-                        onChange(option);
-                    }
-                }
-            }}
-            filterOptions={createFilterOptions<string>({
-                ignoreCase: true,
-                ignoreAccents: true,
-                trim: true,
-                stringify: (option) => option,
-            })}
-            onInputChange={(_, value) => {
-                debouncedChangeHandler(value);
-            }}
-            loading={loading}
             size="small"
-            fullWidth
-            options={options?.map((option) => option.displayName ?? '') ?? []}
+            loading={loading}
+            options={options ?? []}
+            filterOptions={(opts) => opts}
+            onChange={(_, value) => (value && typeof value === 'object' ? onChange(value) : null)}
+            onInputChange={(_, value, reason) => {
+                return reason === 'input' ? debouncedInputHandler(value) : onChange(null);
+            }}
+            getOptionLabel={(option) =>
+                typeof option === 'string' ? option : option.displayName ?? ''
+            }
             renderInput={(params) => <TextField {...params} label={label} variant="outlined" />}
         />
     );
