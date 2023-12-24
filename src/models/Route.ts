c73c89bd00da878663e4c89b1ae7500a3804c8f3
elias-ka <48407293+elias-ka@users.noreply.github.com>
2023-12-24 21:12:48 +0200

    Add nonnegative validation for distance and duration in Route schema

--- src/models/Route.ts
+++ src/models/Route.ts
@@ -4,7 +4,8 @@ export const routeSchema = z.object({
     geometry: z.object({
         coordinates: z.array(z.array(z.number()).length(2)),
     }),
-    distance: z.number(),
+    distance: z.number().nonnegative(),
+    duration: z.number().nonnegative(),
 });
 
 export type Route = z.infer<typeof routeSchema>;
